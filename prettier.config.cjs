0056c3d634a04b2d781eb1c455fe379bf560174f
czechaj <cevdetziyaerdogan@outlook.com>
2023-06-12 19:20:58 +0300

    navbar and auth logic on navbar

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve(
+      "prettier-plugin-tailwindcss"
+    ),
+  ],
+  printWidth: 120,
 };
 
 module.exports = config;
